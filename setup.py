17e284e13f8e893bc96c5bb904b29caa6da41340
f0rkz <f0rkz@f0rkznet.net>
2017-03-25 22:47:13 -0400

    Bump the minor version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pysteamcmd',
-      version='0.1.1',
+      version='0.1.2',
       description='Python package to install and utilize steamcmd',
       url='http://github.com/f0rkz/pysteamcmd',
       author='f0rkz',
